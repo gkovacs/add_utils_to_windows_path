4de2df7a9a467c89d4d91a85d9bd22a1a5b5d9cd
Geza Kovacs <geza@cs.stanford.edu>
2015-03-16 03:49:17 -0700

    add paths to powershell profile

--- add_utils_to_windows_path.ps1
+++ add_utils_to_windows_path.ps1
@@ -24,9 +24,36 @@ function mkbatfile($source, $targetname) {
   }
 }
 
+function addpathifneeded($source, $targetname) {
+  if ((get-command "$targetname").path.length -gt 0) {
+    echo "$targetname already in path"
+    return
+  }
+  if (-not (test-path "$source")) {
+    echo "$source not found"
+    return
+  }
+  $dirpath = (get-childitem $source).directory.fullname
+  $profiledir = (get-childitem $source).directory.fullname
+  if (-not (test-path "$profiledir")) {
+    mkdir "$profiledir"
+  }
+  $env:Path += ";$dirpath"
+  echo "`$env:Path += `"`;$dirpath`"" | out-file -encoding ASCII -append "$profile"
+  # $profile # is the file pointing to the profile
+  # add to it the line
+  # $env:Path += ";C:\path\to\somepath\bin"
+  # or
+  # $env:Path = $env:Path + ";C:\path\to\somepath\bin"
+}
+
 mkbatfile "c:\program files\sublime text 3\subl.exe" "subl"
-mkbatfile "c:\program files\git\bin\git.exe" "git"
-mkbatfile "c:\program files\git\bin\ssh.exe" "ssh"
-mkbatfile "c:\program files\git\bin\bash.exe" "bash"
-mkbatfile "c:\mingw\msys\1.0\bin\rsync.exe" "rsync"
+#mkbatfile "c:\program files\git\bin\git.exe" "git"
+#mkbatfile "c:\program files\git\bin\ssh.exe" "ssh"
+#mkbatfile "c:\program files\git\bin\bash.exe" "bash"
+#mkbatfile "c:\mingw\msys\1.0\bin\rsync.exe" "rsync"
+addpathifneeded "c:\program files\git\bin\git.exe" "git"
+addpathifneeded "c:\program files\git\bin\ssh.exe" "ssh"
+addpathifneeded "c:\program files\git\bin\bash.exe" "bash"
+addpathifneeded "c:\mingw\msys\1.0\bin\rsync.exe" "rsync"
 pause
